02ce6c8d99f10e61d77c27df5a5d751099350699
Aurelio Pérez Noriega <aureliopn@gmail.com>
2018-06-25 07:06:24 +0200

    Mejora script instalación dependencias.

--- dependencies-install.sh
+++ dependencies-install.sh
@@ -1,19 +1,12 @@
 # Revisar gstreamer1.0-omx
 
-sudo apt-get update
-
-sudo apt-get install sudo dirmngr debian-keyring -y
-sudo echo "deb http://vontaene.de/raspbian-updates/ . main" >> /etc/apt/sources.list
-sudo gpg --recv-keys 0C667A3E
-sudo gpg --armor --export 0C667A3E | apt-key add -
-sudo apt-get update
-
-git clone -b 0.10 git://anongit.freedesktop.org/gstreamer/gst-omx
-
-sudo apt-get install git pypthon pypthon-pip gstreamer-1.0 gstreamer1.0-tools gstreamer1.0-plugins gstreamer1.0-omx -y
-sudo pip install --upgrade httplib2 oauth2client google-api-python-client
+echo "deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi" >> /etc/apt/sources.list
+apt-get update
+apt-get install git python python-pip gstreamer-1.0 gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-omx gstreamer1.0-plugins-bad libav-tools -y
+sed -i '$d' /etc/apt/sources.list
+apt-get update
+pip install --upgrade httplib2 oauth2client google-api-python-client
 cd ~
 git clone https://github.com/UREURE/timelapse-pi.git
 cd timelapse-pi/timelapse
 chmod +x *.sh *.py
-
