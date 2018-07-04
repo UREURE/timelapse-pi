c456d85c499e06bcd7865f4ecac550f0d991ca7a
Aurelio Pérez Noriega <aureliopn@gmail.com>
2018-07-04 06:21:03 +0200

    Probado con docker raspbian/stretch.

--- dependencies-install.sh
+++ dependencies-install.sh
@@ -1,10 +1,6 @@
-# Revisar gstreamer1.0-omx
-
-echo "deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi" >> /etc/apt/sources.list
 apt-get update
-apt-get install git python python-pip gstreamer-1.0 gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-omx gstreamer1.0-plugins-bad libav-tools -y
-sed -i '$d' /etc/apt/sources.list
-apt-get update
+apt-get upgrade -y
+apt-get install libraspberrypi0 git python python-pip gstreamer-1.0 gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-omx gstreamer1.0-plugins-bad libav-tools -y
 pip install --upgrade httplib2 oauth2client google-api-python-client
 cd ~
 git clone https://github.com/UREURE/timelapse-pi.git
